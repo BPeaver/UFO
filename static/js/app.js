6b600f136e4f1ed5c15b47ce935f42d05b70535e
Brian P <b.peaver@gmail.com>
2022-03-13 22:29:18 -0400

    building table

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,15 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+function buildTable(data) {
+    tbody.html(""); // sets the table to an empty string to clear it for data
+  }
+
+  data.forEach((dataRow) => { // sets a for loop to go through each row of data
+    let row = tbody.append("tr"); // declares row variable, tells JS to find <tbody> tags and add row to table 'tr'
+    Object.values(dataRow).forEach((val) => { // tells JS to reference one object from UFO array, adds values to dataRow, and the .forEach() specifies 1 value per row
+      let cell = row.append("td"); // appends data into table data tag <td>
+      cell.text(val); // variable "cell" holds only the text value from the object
+      }
+    );
+  });
